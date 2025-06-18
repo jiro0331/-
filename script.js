5fef71b336b5063c68bee860bbd7303f6f9685b3
jiro0331 <s241018@kashiwanoha.ed.jp>
2025-06-18 07:05:58 +0000

    Update page

--- script.js
+++ script.js
@@ -1,45 +1,20 @@
 const wordList = [ 
-
 { japanese: "関数型プログラミングがソフトウェア開発者の間で注目されている。", english: "Functional programming has been gaining popularity among software developers." }, 
-
 { japanese: "走る", english: "run" }, 
-
 { japanese: "大きい", english: "big" } 
-
 ]; 
-
- 
-
 function generateWordList() { 
-
 const container = document.getElementById("wordContainer"); 
-
- 
-
 wordList.forEach((word, index) => { 
-
-const block = document.createElement("div"); 
-
- 
-
+const block = document.createElement("div");
 // 日本語表示 
-
 const jp = document.createElement("p"); 
-
 jp.textContent = `意味：${word.japanese}`; 
-
 block.appendChild(jp); 
-
- 
-
 // 英語表示ボタン 
-
 const button = document.createElement("button"); 
-
 button.textContent = "英語を見る・聞く"; 
-
 button.onclick = () => showAnswer(index); 
-
 block.appendChild(button); 
 
  
